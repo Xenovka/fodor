65016f48730cc45247ab4c9325d4cf8f9e1a8ca8
Wendy <wendyanto.bukitvista@gmail.com>
2024-04-05 16:29:55 +0700

    sharing data between component using React useContext API

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from "expo-status-bar";
+import { useContext } from "react";
 import { View, Text, Platform } from "react-native";
+import { useCart } from "../providers/CartProvider";
 
 const CartScreen = () => {
+    const { items } = useCart();
+
     return (
         <View>
-            <Text>CartScreen</Text>
+            <Text>Cart item length: {items.length}</Text>
 
             <StatusBar style={Platform.OS === "android" ? "light" : "auto"} />
         </View>
