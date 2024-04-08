4cbf3d29cb9e0352491346942b0353232eb3c7d6
Wendy <wendyanto.bukitvista@gmail.com>
2024-04-08 11:33:19 +0700

    managing user auth

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,13 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
 import { useColorScheme } from "@/src/components/useColorScheme";
 import CartProvider from "../providers/CartProvider";
+import AuthProvider, { useAuth } from "../providers/AuthProvider";
 
 export {
     // Catch any errors thrown by the Layout component.
@@ -50,14 +51,16 @@ function RootLayoutNav() {
 
     return (
         <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-            <CartProvider>
-                <Stack>
-                    <Stack.Screen name="(admin)" options={{ headerShown: false }} />
-                    <Stack.Screen name="(user)" options={{ headerShown: false }} />
-                    <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-                    <Stack.Screen name="cart" options={{ presentation: "modal" }} />
-                </Stack>
-            </CartProvider>
+            <AuthProvider>
+                <CartProvider>
+                    <Stack>
+                        <Stack.Screen name="(admin)" options={{ headerShown: false }} />
+                        <Stack.Screen name="(user)" options={{ headerShown: false }} />
+                        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+                        <Stack.Screen name="cart" options={{ presentation: "modal" }} />
+                    </Stack>
+                </CartProvider>
+            </AuthProvider>
         </ThemeProvider>
     );
 }
